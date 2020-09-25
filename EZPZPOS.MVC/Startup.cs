e963a341e5c882177c2d5e9822b6631249b9085d
seansql <shawnq2116@gmal.com>
2020-09-25 16:17:04 -0400

    Add project files.

---
+++ EZPZPOS.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EZPZPOS.MVC.Startup))]
+namespace EZPZPOS.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
